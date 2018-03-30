4f623b0ac2e7448664e1c9e2c1be8389540e8cfd
Khasanov Amir <aworldx@gmail.com>
2018-03-30 10:10:35 +0000

    fix violations

--- movies.rb
+++ movies.rb
@@ -1,31 +1,14 @@
 def perform_movie_lib(file_name)
   lib = Array.new
+  keys = [:link, :title, :year, :country, :premiere_date, :genre, :durability, :rate, :producer]
 
   File.open(file_name) do |fp|
     fp.each do |line|
-      movie_attrs = line.split('|')
-      
-      h = {
-        link: movie_attrs[0],
-        title: movie_attrs[1],
-        year: movie_attrs[2],
-        country: movie_attrs[3],
-        premiere_date: movie_attrs[4],
-        genre: movie_attrs[5],
-        durability: number_from_string(movie_attrs[6]),
-        rate: movie_attrs[7],
-        producer: movie_attrs[8].split.last
-      }
-
-      lib << h
+      lib << Hash[keys.zip(line.split('|'))]
     end
   end
 
   lib
-end
-
-def number_from_string(string)
-  string.scan( /\d+/ ).first.to_i
 end
 
 def show_movies(movies)
@@ -43,10 +26,10 @@ end
 
 movie_lib = perform_movie_lib(file_name)
 puts '5 longest movies'
-show_movies(movie_lib.sort_by { |m| -m[:durability] }.first(5))
+show_movies(movie_lib.sort_by { |m| -m[:durability].to_i }.first(5))
 puts '10 comedies'
 show_movies(movie_lib.select { |m| m[:genre].include?('Comedy') }.sort_by { |m| m[:premiere_date] }.first(10))
 puts 'producers'
-puts movie_lib.map { |m| m[:producer] }.uniq.sort
+puts movie_lib.map { |m| m[:producer].split.last }.uniq.sort
 print 'foreign movies count '
 puts movie_lib.reject { |m| m[:country].include?('USA') }.size
