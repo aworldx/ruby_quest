70317ebac0091185a751d1af515b175bd69b662c
Khasanov Amir <aworldx@gmail.com>
2018-03-30 11:25:41 +0000

    fourth-task

--- movies.rb
+++ movies.rb
@@ -1,11 +1,12 @@
+require 'csv'
+require 'ostruct'
+
 def perform_movie_lib(file_name)
   lib = Array.new
-  keys = [:link, :title, :year, :country, :premiere_date, :genre, :durability, :rate, :producer]
+  keys = [:link, :title, :year, :country, :premiere_date, :genre, :durability, :rate, :producer, :actors]
 
-  File.open(file_name) do |fp|
-    fp.each do |line|
-      lib << Hash[keys.zip(line.split('|'))]
-    end
+  CSV.foreach(file_name, {headers: keys, col_sep: '|'}) do |row|
+    lib << OpenStruct.new(row.to_hash)
   end
 
   lib
@@ -13,7 +14,7 @@ end
 
 def show_movies(movies)
   movies.each do |m|
-    puts "#{m[:title]} (#{m[:premiere_date]}; #{m[:genre]}) - #{m[:durability]}"
+    puts "#{m.title} (#{m.premiere_date}; #{m.genre}) - #{m.durability}"
   end
 end
 
@@ -26,10 +27,10 @@ end
 
 movie_lib = perform_movie_lib(file_name)
 puts '5 longest movies'
-show_movies(movie_lib.sort_by { |m| -m[:durability].to_i }.first(5))
+show_movies(movie_lib.sort_by { |m| -m.durability.to_i }.first(5))
 puts '10 comedies'
-show_movies(movie_lib.select { |m| m[:genre].include?('Comedy') }.sort_by { |m| m[:premiere_date] }.first(10))
+show_movies(movie_lib.select { |m| m.genre.include?('Comedy') }.sort_by { |m| m.premiere_date }.first(10))
 puts 'producers'
-puts movie_lib.map { |m| m[:producer].split.last }.uniq.sort
+puts movie_lib.map { |m| m.producer.split.last }.uniq.sort
 print 'foreign movies count '
-puts movie_lib.reject { |m| m[:country].include?('USA') }.size
+puts movie_lib.reject { |m| m.country.include?('USA') }.size
