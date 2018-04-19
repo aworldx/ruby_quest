dbf4d86a43132363938f62aae00e477c2b1249f2
Khasanov Amir <aworldx@gmail.com>
2018-04-19 16:07:43 +0000

    fix violations

--- cashbox.rb
+++ cashbox.rb
@@ -3,14 +3,14 @@ module Cashbox
     self.balance += format_amount(money)
   end
 
-  def format_amount(money)
-    money.is_a?(Money) ? money : Money.new(100 * money, "USD")  
+  def format_amount(amount)
+    Money.new(amount, "USD") * 100 
   end
 
   def take(who)
     if who == "Bank"
       puts 'encashment'
-      self.balance = Money.new(0, "USD")
+      self.balance = format_amount(0)
     else
       raise 'u-u-u-u-u-u-u'
     end
